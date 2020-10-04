7f3f0b2d2b7bd1b509251bf8bdeadd6749b5c945
Bob <maydaybob2000@gmail.com>
2020-10-04 23:02:10 +0800

    Add some style to Pick.js

--- src/Pick.js
+++ src/Pick.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import firebase from 'firebase'
 import { useParams } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
 import itemData from './data/itemData.json'
 import Button from '@material-ui/core/Button'
 import { shuffleArray } from './lib/helper'
@@ -9,7 +10,32 @@ import { shuffleArray } from './lib/helper'
 // 列出全部成員
 // 按三下(冒愛心) => 選取
 
+const useStyles = makeStyles({
+  root: {
+    textAlign: 'center',
+  },
+  member: {
+    float: 'left',
+    width: '50%',
+    position: 'relative',
+    cursor: 'pointer',
+  },
+  memberPhoto: {
+    width: '100%',
+  },
+  memberName: {
+    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 800,
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+})
+
 const Pick = () => {
+  const classes = useStyles()
   const { gameDoc, cardDoc } = useParams()
   const db = firebase.firestore()
   const cardRef = db.collection('games').doc(gameDoc).collection('cards').doc(cardDoc)
@@ -55,17 +81,21 @@ const Pick = () => {
 
   let memberImgList = memberSet.map((item) => {
     return (
-      <img
-        key={item.name}
-        src={item.photoURL}
-        alt={'alt'}
-        onClick={() => handleImgOnClick(item.name, item.photoURL)}></img>
+      <div className={classes.member}>
+        <img
+          className={classes.memberPhoto}
+          key={item.name}
+          src={item.photoURL}
+          alt={'alt'}
+          onClick={() => handleImgOnClick(item.name, item.photoURL)}></img>
+        <div className={classes.memberName}>{item.name}</div>
+      </div>
     )
   })
 
   useEffect(() => {
     cardRef.get().then(function (doc) {
-      if (doc.data() && doc.data()['state'] === 0) {
+      if (doc.data() && doc.data()['state'] <= 1) {
         setIsPickable(true)
       } else {
         setIsPickable(false)
@@ -74,7 +104,7 @@ const Pick = () => {
   }, [])
 
   return (
-    <div>
+    <div className={classes.root}>
       Pick~~~ {isComplete && <h1>Good Job!</h1>}
       <div>
         <h2>gameDoc: {gameDoc}</h2>
