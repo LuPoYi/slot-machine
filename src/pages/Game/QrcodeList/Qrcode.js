7b224093fa915ffe5016a1703af847bceffb3c75
Bob <maydaybob2000@gmail.com>
2020-10-10 17:33:32 +0800

    Fix qrcode url

--- src/pages/Game/QrcodeList/Qrcode.js
+++ src/pages/Game/QrcodeList/Qrcode.js
@@ -16,13 +16,14 @@ const useStyles = makeStyles({
 
 const Qrcode = ({ gameDoc, cardDoc, state, photoURL, isShowPickURL }) => {
   const classes = useStyles()
+  const url = `${window.location.host}/pick/${gameDoc}/${cardDoc}`
 
   return (
     <div className={classes.root}>
-      {state === 0 && <QRCode value={cardDoc} />}
-      {state === 1 && <QRCode value={cardDoc} fgColor={'#CCCCCC'} />}
+      {state === 0 && <QRCode value={url} />}
+      {state === 1 && <QRCode value={url} fgColor={'#CCCCCC'} />}
       {state === 2 && <img width="128" height="128" src={photoURL} alt={'alt'}></img>}
-      {isShowPickURL && <p>{`${window.location.host}/pick/${gameDoc}/${cardDoc}`}</p>}
+      {isShowPickURL && <p>{url}</p>}
     </div>
   )
 }
