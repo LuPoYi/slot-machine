ccec5458bbaf3224854304c63f613e81bf0220bd
Bob <maydaybob2000@gmail.com>
2020-10-24 19:26:45 +0800

    Add Select

--- src/pages/Game/Welcome/Welcome.js
+++ src/pages/Game/Welcome/Welcome.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import firebase from '../../../utils/firebase'
 import Button from '@material-ui/core/Button'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
 import { useHistory } from 'react-router-dom'
 
 const Welcome = ({ count }) => {
   let history = useHistory()
+  const [group, setGroup] = React.useState('')
 
   const generateNewGame = () => {
     let isError = false // TODO promise
@@ -50,11 +54,28 @@ const Welcome = ({ count }) => {
     }
   }
 
+  const handleChange = (event) => {
+    setGroup(event.target.value)
+  }
+
   return (
     <div>
       <h1>Welcome!</h1>
+      <div>
+        <InputLabel id="demo-simple-select-label">group</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={group}
+          onChange={handleChange}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </div>
+
       <Button variant="contained" color="primary" onClick={generateNewGame}>
-        Start
+        Start~
       </Button>
     </div>
   )
